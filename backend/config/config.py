87aba67d100b389f55cbc2276f3add2e8cac5bf8
Nmait2111 <namitjain2111@gmail.com>
2024-09-03 19:38:30 +0530

    added gemini and config

---
+++ backend/config/config.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+gemini_api = os.getenv("GEMINI_API")
---
+++ backend/utils/gemini.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai
+from config.config import gemini_api
+import json
+
+genai.configure(api_key=gemini_api)
+
+
+#to generate json response
+model_json = genai.GenerativeModel('gemini-1.5-flash',generation_config={"response_mime_type": "application/json"})
+def generate_json_content(prompt):
+    respone = model_json.generate_content(prompt)
+    response_json = json.loads(response.text)
+    return respone_json
+
+#to generate text response
+model_text = genai.GenerativeModel('gemini-1.5-flash')
+def generate_content(prompt):
+
+    respone = model.generate_content(prompt)
+    return response.text
